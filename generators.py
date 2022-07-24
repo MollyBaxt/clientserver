b9c831e380552dc1783b96b816fb7e36bd1bbc03
Karl Davis <karldavis@me.com>
2022-07-24 10:12:11 +0100

    added random generator functions for dicts and text

---
+++ generators.py
@@ -0,0 +1,39 @@
+from random import randint
+from time import time
+from crypto import encrypt_data
+import lorem
+
+
+def generate_dict():
+    # """Generate random json packet with hashed data bits"""
+    return {
+        "id": randint(1, 100),
+        "timestamp": time(),
+        "data": hash(str(randint(1, 100)))
+    }
+
+
+def generate_plaintext():
+    t = lorem.sentence()
+    print(f"Text Generated:\n"
+          f"{t}")
+
+    with open('lorem_plaintext.txt', 'w') as f:
+        f.write(t)
+        f.close()
+
+    return 'lorem_plaintext'
+
+
+def generate_crytpo_text():
+    t = encrypt_data(lorem.sentence())
+    print(t)
+
+    print(f"Text Generated:\n"
+          f"{t}")
+
+    with open('lorem_encrypted.txt', 'w') as f:
+        f.write(str(t))
+        f.close()
+
+    return 'lorem_encrypted'
