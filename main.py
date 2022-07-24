029e7659bb0b7c2e6fa2d660bbf8589fbdf9ba6f
Karl Davis <karldavis@me.com>
2022-07-24 10:17:36 +0100

    intro to program

---
+++ main.py
@@ -0,0 +1,30 @@
+import sys
+from file_handler import generate_data
+
+print("Client/Server and Encryption Program")
+print("------------------------------------")
+print("Options :")
+print("1: Generate random text and send to server")
+print("2: Generate random text, encrypt and send to server")
+print("3: Generate random JSON data and send to server")
+print("4: Exit")
+print("------------------------------------")
+
+
+while True:
+    try:
+        user_sel = int(input("Please make a selection: "))
+    except ValueError:
+        print("Please ensure you have entered a valid number")
+    else:
+        if user_sel == 1 or user_sel == 2 or user_sel == 3:
+            generate_data(user_sel)
+        elif user_sel == 4:
+            print("Exiting...")
+            sys.exit()
+        else:
+            print("Please enter within the range provided")
+
+
+
+
