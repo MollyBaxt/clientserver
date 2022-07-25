32323de81194c6c8feecab7025bb76c31a253171
Karl Davis <karldavis@me.com>
2022-07-25 21:40:17 +0100

    Shifted IP

--- src/client.py
+++ src/client.py
@@ -1,31 +1,34 @@
 import socket
-import sys
+
+IP = "127.0.0.1"
+PORT = 1234
+ADDR = (IP, PORT)
+FORMAT = "utf-8"
+SIZE = 1024
 
 
 def client_textfile(file_name):
-    file = open(f"{file_name}.txt", "r")
+    file = open(f"data/client_data/{file_name}", "r")
     data = file.read()
     client_connection(data)
+    file.close()
 
 
-def client_connection(data):
+def client_connection(file_name, data):
+    print(file_name, data)
+    """ Staring a TCP socket. """
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    """ Connecting to the server. """
+    client.connect(ADDR)
+    """ Sending the filename to the server. """
+    client.send(file_name.encode(FORMAT))
+    msg = client.recv(SIZE).decode(FORMAT)
+    print(f"[SERVER]: {msg}")
+    """ Sending the file data to the server. """
+    client.send(data.encode(FORMAT))
+    msg = client.recv(SIZE).decode(FORMAT)
+    print(f"[SERVER]: {msg}")
+    """ Closing the file. """
+    """ Closing the connection from the server. """
+    client.close()
 
-    try:
-        sock = socket.socket()
-    except socket.error as err:
-        print('Socket error because of %s' % err)
-
-    port = 11111
-    host = "localhost"
-
-    try:
-        sock.connect((host, port))
-        sock.send(bytes(data, 'utf-8'))
-    except socket.gaierror:
-
-        print('There an error resolving the host')
-
-        sys.exit()
-
-    print(data, 'was sent!')
-    sock.close()
